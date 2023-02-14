af890b92d882920eb04d4b467ce79969672e60cd
Arun Pandiyan <41441589+LonelyKing26@users.noreply.github.com>
2023-02-14 17:36:17 +0530

    Add files via upload

---
+++ test.php
@@ -0,0 +1,75 @@
+<!-- <form action="get-post.php" method="POST">
+	<input type="text" name="data" value="3" hidden>
+    <input type="submit"/>
+</form> -->
+<style>
+	span.file-name.file-name-without-size
+	{
+		display:none;
+		font-size:2px;
+		color:#000;
+	}
+</style>
+<policy-directive>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<video controls src="magnet:?xt=urn:btih:afa40591cca262144ed830ab8463d7b644118d82" data-path="www.1TamilMV.bond - Vedha (2022) HQ HDRip - x264 - (AAC 2.0) [Tamil + Telugu] - 450MB - ESub.mkv"></video>
+<!-- <video controls src="magnet:?xt=urn:btih:afa40591cca262144ed830ab8463d7b644118d82&dn=www.1TamilMV.bond+-+Vedha+(2022)+HQ+HDRip+-+x264+-+(AAC+2.0)+%5BTamil+%2B+Telugu%5D+-+450MB+-+ESub.mkv&tr=http%3A%2F%2Ftracker.trackerfix.com%3A80%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2850%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2970%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.slowcheetah.org%3A14760%2Fannounce&tr=udp%3A%2F%2Ftracker.thinelephant.org%3A12780%2Fannounce" data-path="www.1TamilMV.bond - Vedha (2022) HQ HDRip - x264 - (AAC 2.0) [Tamil + Telugu] - 450MB - ESub.mkv"></video>
+<script src="https://cdn.jsdelivr.net/npm/@webtor/player-sdk-js/dist/index.min.js" charset="utf-8" async></script> -->
+
+<!-- https://abra--67910d27.api.frosty-night.buzz/afa40591cca262144ed830ab8463d7b644118d82/www.1TamilMV.bond%20-%20Vedha%20(2022)%20HQ%20HDRip%20-%20x264%20-%20(AAC%202.0)%20%5BTamil%20%2B%20Telugu%5D%20-%20450MB%20-%20ESub.mkv?user-id=bae2cbfbcc3ef955b6f63d4352501979&download=true&download-id=68096c2c2104ec46b2d89188443f6d57&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZ2VudCI6Ik1vemlsbGEvNS4wIChXaW5kb3dzIE5UIDEwLjA7IFdpbjY0OyB4NjQpIEFwcGxlV2ViS2l0LzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIENocm9tZS8xMDkuMC4wLjAgU2FmYXJpLzUzNy4zNiBPUFIvOTUuMC4wLjAiLCJyZW1vdGVBZGRyZXNzIjoiMjQwNToyMDE6ZTAxZjoyZTQ1OjNjYWE6N2FkODoyYzI6N2JjNyIsImRvbWFpbiI6IndlYnRvci5pbyIsImV4cCI6MTY3Njk4MDY1Mywic2Vzc2lvbklEIjoiaVV5LWNfM2s4VlhGbnFBWGRULWZGQy1wdVNHOEtLX0oiLCJyYXRlIjoiMTBNIiwicm9sZSI6Im5vYm9keSJ9.7xhawCiEjjK3iQgdTUU8isS5cDRx5irMsvF6NOCNWuM&api-key=8acbcf1e-732c-4574-a3bf-27e6a85b86f1 -->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@webtor/player-sdk-js/dist/index.min.js" charset="utf-8" async></script>
+<?php
+	// include('datab.php');
+	// $jobstats = [];
+	// $sql = mysqli_query($conn, "SELECT job_status 
+	// 							FROM product_sales_data 
+	// 							WHERE status = 'Active'");
+
+	// while($job = mysqli_fetch_assoc($sql))
+	// {
+	// 	$jobstats[]['data'] = $job;
+	// }
+
+	// $target = array('Delivery');
+	
+	// $jonnn = count(array_intersect($jobstats[0]['job_status'], $target));
+
+	// echo $jobstats;
+	// print_r($jobstats);
+	// $jsencode = json_encode($jobstats);
+	// echo $jsencode;
+	// 0 = tomorr
+	// -1 = today
+	// -2 = last day
+	// date_default_timezone_set('Asia/Kolkata');
+	// $now = time(); // or your date as well
+	// $your_date = strtotime("2023-02-10");
+	// $datediff = $your_date - $now;
+
+	// echo round($datediff / (60 * 60 * 24));
+	// echo $now;
+
+
+?>
+<style>
+	.list-group-item-action
+	{
+		display: none !important;
+		color: #000;
+	}
+</style>
+<script>
+	//  document.getElementsByClassName("file-name").innerHTML = "I have changed!";
+        $(document).ready(function(){
+			// $('body').on(".file-name-without-size", function(){
+			// 	$(".file-name-without-size").html("testt");
+			// });
+        //     $("._4KjPzfFqnPyBgIgiXkX").addClass("d-none");
+        //    $(".file-name").addClass("d-none");
+            // $(".n9VUXQmB98rW0ML_Bznz").css("display", "none");
+        });
+</script>
